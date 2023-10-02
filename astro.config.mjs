f8cc2c8ce79e289d2ca50d17aeaa445075975cf8
PorcoRosso85 <1.is.universe@gmail.com>
2023-10-03 07:18:48 +0900

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://porcorosso85.github.io",
+  base: "/portfolio",
   integrations: [
     react({
       experimentalReactChildren: true,
