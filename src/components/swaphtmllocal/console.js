b11ea99960260e9adcffa87ea0499c78cbafe2bd
PorcoRosso85 <1.is.universe@gmail.com>
2023-10-04 14:01:14 +0900

    update hx swap local

---
+++ src/components/swaphtmllocal/console.js
@@ -0,0 +1,5 @@
+console.log("LocalStorage newElement:", localStorage.getItem("newElement"));
+
+document.getElementById("oldElement").addEventListener("click", function () {
+  console.log("クリックされました");
+});
--- src/components/swaphtmllocal/swapLocal.js
+++ src/components/swaphtmllocal/swapLocal.js
@@ -1,16 +1,8 @@
 console.log("index.js 読み込み"); // スクリプトが読み込まれているか確認
 
-htmx.defineExtension("swapFromLocalStorage", {
+htmx.defineExtension("swapLocal", {
   onEvent: function (name, evt) {
     console.log("イベント名: " + name); // イベント名を出力
-    // if (name === "click") {
-    //   const newElement = localStorage.getItem("newElement");
-    //   console.log("新しい要素: " + newElement); // 新しい要素を出力
-    //   if (newElement) {
-    //     evt.target.innerHTML = newElement;
-    //     htmx.process(evt.target);
-    //   }
-    // }
     if (name === "htmx:trigger") {
       const targetElement = document.getElementById("oldElement"); // 明示的に要素を取得
       const newElement = localStorage.getItem("newElement");
@@ -18,7 +10,6 @@ htmx.defineExtension("swapFromLocalStorage", {
       console.log("新しい要素: " + newElement);
 
       if (newElement && targetElement) {
-        // targetElementも確認
         targetElement.innerHTML = newElement;
       }
     }
